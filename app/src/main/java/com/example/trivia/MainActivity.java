d29f0dbf2f0ccc54af082670dbb7007fb4cf7458
User <Tanzilbd20@gmail.com>
2021-06-14 23:07:45 +0800

    added new score option; true false score; saved last highest score;

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -4,36 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.example.trivia.controller.AppController;
-import com.example.trivia.data.AnswerListAsyncResponse;
 import com.example.trivia.data.Repository;
 import com.example.trivia.databinding.ActivityMainBinding;
 import com.example.trivia.model.Questions;
 import com.google.android.material.snackbar.Snackbar;
 
-import org.json.JSONArray;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String MESSAGE_ID = "message_prefs";
     private ActivityMainBinding binding;
-    private int currentQuestion = 0;
+    private int currentQuestion = 1;
     List<Questions> questions;
+    private int correct_score = 0;
+    private int wrong_score = 0;
 
     @SuppressLint("DefaultLocale")
     @Override
@@ -46,17 +37,22 @@ public class MainActivity extends AppCompatActivity {
 
                 binding.questionTextView.setText(questionsArrayList.get(currentQuestion).getAnswer());
                 updateQuestionCounter();
+                getHighestScore();
 
             }
       );
 
         binding.trueButton.setOnClickListener(view -> {
             checkAnswer(true);
+            binding.trueButton.setEnabled(false);
+            binding.falseButton.setEnabled(false);
             updateQuestion();
 
         });
         binding.falseButton.setOnClickListener(view -> {
             checkAnswer(false);
+            binding.trueButton.setEnabled(false);
+            binding.falseButton.setEnabled(false);
             updateQuestion();
 
 
@@ -64,8 +60,10 @@ public class MainActivity extends AppCompatActivity {
 
         binding.nextButton.setOnClickListener(view -> {
             currentQuestion = (currentQuestion +1)  %questions.size() ;
-
+            binding.trueButton.setEnabled(true);
+            binding.falseButton.setEnabled(true);
             updateQuestion();
+
 
         });
     }
@@ -82,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @SuppressLint("DefaultLocale")
     private void checkAnswer(boolean userChoice) {
 
         boolean answerIs = questions.get(currentQuestion).isAnswerTrue();
@@ -89,15 +88,21 @@ public class MainActivity extends AppCompatActivity {
 
         if(userChoice == answerIs){
             snackMessageId = R.string.correct_answer;
+            correct_score++;
+            correctScoreAnimation();
+            setHighestScore();
 
            alphaAnimation();
         }else{
             snackMessageId = R.string.incorrect_answer;
+            wrong_score++;
+            wrongScoreAnimation();
            shakeAnimation();
 
-
-
         }
+
+        binding.correctAnswerTextView.setText(String.format("Correct Answer\n%d", correct_score));
+        binding.wrongAnswerTextView.setText(String.format("Wrong Answer\n%d", wrong_score));
         Snackbar.make(binding.cardView,snackMessageId,Snackbar.LENGTH_SHORT).show();
 
     }
@@ -141,9 +146,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void translatedAnimation(){
-        Animation translated = AnimationUtils.loadAnimation(this,R.anim.translated_animation);
-        binding.cardView.setAnimation(translated);
+    private void correctScoreAnimation(){
+        Animation scoreAnimation = AnimationUtils.loadAnimation(this,R.anim.alpha_score_animation);
+        binding.correctAnswerTextView.setAnimation(scoreAnimation);
     }
+    private void wrongScoreAnimation(){
+        Animation scoreAnimation = AnimationUtils.loadAnimation(this,R.anim.alpha_score_animation);
+        binding.wrongAnswerTextView.setAnimation(scoreAnimation);
+    }
+    private void setHighestScore(){
+        String highestScore = String.valueOf(correct_score);
+        SharedPreferences sharedPreferences = getSharedPreferences(MESSAGE_ID,MODE_PRIVATE);
+        @SuppressLint("CommitPrefEdits") SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("score",highestScore);
+        editor.apply();
+    }
+    private void getHighestScore(){
+        SharedPreferences sharedPreferences = getSharedPreferences(MESSAGE_ID,MODE_PRIVATE);
+       String score =  sharedPreferences.getString("score","Last Highest Score : 0");
+       binding.highestScoreTextView.setText(String.format("Last Highest Score : %s", score));
+    }
+
+
+
 
 }
